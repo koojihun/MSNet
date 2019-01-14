f3eb25a92796818a835d5d1481ee470f1c322c66
HwangBoSungHun <thehb01@gmail.com>
2019-01-14 15:50:27 +0900

    Connect server and login view

--- com/msnet/MainApp.java
+++ com/msnet/MainApp.java
@@ -1,16 +1,15 @@
 package com.msnet;
 
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
+import com.bitcoinClient.javabitcoindrpcclient.BitcoinJSONRPCClient;
 import com.msnet.util.Bitcoind;
 import com.msnet.util.HTTP;
 import com.msnet.util.Settings;
 import com.msnet.view.LoginViewController;
 import com.msnet.view.SystemOverviewController;
-import com.bitcoinClient.javabitcoindrpcclient.BitcoinJSONRPCClient;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
@@ -24,96 +23,98 @@ import javafx.util.Duration;
 
 public class MainApp extends Application {
 
-	private Stage primaryStage;
+	static public Stage primaryStage;
+	static private BorderPane rootLayout;
 	static public BitcoinJSONRPCClient bitcoinJSONRPClient;
-	
-	/*
-	public void initRootLayout() {
-		
+
+	public static void initRootLayout() {
 		try {
 			// fmxl 파일에서 상위 레이아웃을 가져온다.
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-			rootLayout = (BorderPane)loader.load();
-			
+			rootLayout = (BorderPane) loader.load();
+
 			// 상위 레이아웃을 포함하는 scene을 보여준다.
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}	
-		
+		}
 	}
-	
-	public void showSystemOverview() {
-		
-		AnchorPane systemOverview;
-		
+
+	public static void showSystemOverview() {
 		try {
 			// systemOverview를 fmxl 파일에서 가져온다.
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/SystemOverview.fxml"));
-			systemOverview = (AnchorPane) loader.load();
-			
+			AnchorPane systemOverview = (AnchorPane) loader.load();
+
 			// systemOverview를 상위 레이아웃의 가운데로 설정한다.
 			rootLayout.setCenter(systemOverview);
-			
+
 			// 메인 애플리케이션이 컨트롤러를 이용할 수 있게 한다.
 			SystemOverviewController controller = loader.getController();
-			controller.setMainApp(this);
-			
+			//controller.setMainApp(this);
+
+	
 			// Bitcoin daemon 출력
 			new Bitcoind(controller.getBitcoindTextArea()).start();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
-		
+		}
 	}
-	*/
+
 	public static void main(String[] args) throws Exception {
-		/*
-		try {			
+		try {
 			bitcoinJSONRPClient = new BitcoinJSONRPCClient(Settings.getRpcUser(), Settings.getRpcPassword());
 		} catch (MalformedURLException e) {
 			System.err.println("BitcoinJSONRPCClient Constructor Error!!");
 		}
-		*/
 		launch(args);
-		ArrayList<String> keys = new ArrayList<>();
-		ArrayList<String> vals = new ArrayList<>();
-		keys.add("My Name Is");
-		vals.add("Koo Ji Hoon");
-		HTTP.send("http://localhost:8090/NewSystem_web/test_get.do", "get", keys, vals);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("MSNet");
+		
 		showLoginView();
 	}
-	
+
 	public Stage getPrimaryStage() {
 		return primaryStage;
 	}
 	
+	public static void changeScene(String path) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource(path));
+			
+			Scene nextScene = new Scene(loader.load());
+			
+			primaryStage.setScene(nextScene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void showLoginView() {
-		AnchorPane loginPane;		
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/LoginView.fxml"));
-			loginPane = (AnchorPane) loader.load();
+			AnchorPane loginPane = (AnchorPane) loader.load();
 			FadeTransition ft = new FadeTransition(Duration.millis(2500), loginPane);
 			ft.setFromValue(0);
 			ft.setToValue(1);
 			ft.play();
 			Scene loginScene = new Scene(loginPane);
-			
+
 			primaryStage.setScene(loginScene);
-			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			//primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch (IOException e) {
--- com/msnet/view/LoginViewController.java
+++ com/msnet/view/LoginViewController.java
@@ -5,7 +5,13 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import org.json.simple.JSONObject;
+
+import com.msnet.MainApp;
+import com.msnet.util.HTTP;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -17,13 +23,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class LoginViewController implements Initializable {
-	
+
 	private double x = 0;
 	private double y = 0;
 	private String serverURL = "";
-	
+
 	@FXML
 	private Button signUpButton;
 	@FXML
@@ -32,14 +39,15 @@ public class LoginViewController implements Initializable {
 	private TextField passwordTextField;
 	@FXML
 	private Button enterButton;
-	
-	public LoginViewController() {}
-	
+
+	public LoginViewController() {
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// 참고사항 = 멤버 변수 객체들 생성 및 뷰와 연결후 initialize 함수 실행.
 		String original = signUpButton.getStyle();
-		String pressed = original + "-fx-text-fill: gray;" ;
+		String pressed = original + "-fx-text-fill: gray;";
 		signUpButton.setOnMousePressed(new EventHandler<Event>() {
 
 			@Override
@@ -47,7 +55,7 @@ public class LoginViewController implements Initializable {
 				signUpButton.setStyle(pressed);
 			}
 		});
-		
+
 		signUpButton.setOnMouseReleased(new EventHandler<Event>() {
 
 			@Override
@@ -55,9 +63,9 @@ public class LoginViewController implements Initializable {
 				signUpButton.setStyle(original);
 			}
 		});
-		
+
 		signUpButton.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
 				try {
@@ -71,25 +79,52 @@ public class LoginViewController implements Initializable {
 				}
 			}
 		});
-	} 
-	
+	}
+
 	@FXML
-    void dragged(MouseEvent event) {
+	void dragged(MouseEvent event) {
 		Node node = (Node) event.getSource();
 		Stage stage = (Stage) node.getScene().getWindow();
-		
+
 		stage.setX(event.getScreenX() - x);
 		stage.setY(event.getScreenY() - y);
-    }
+	}
 
-    @FXML
-    void pressed(MouseEvent event) {
-    	x = event.getSceneX();
-    	y = event.getSceneY();
-    }
-    
-    @FXML
-    private void handleEnter() {
-    	
-    }
+	@FXML
+	void pressed(MouseEvent event) {
+		x = event.getSceneX();
+		y = event.getSceneY();
+	}
+
+	@FXML
+	private void handleEnter() {
+
+		String id = idTextField.getText();
+		String password = passwordTextField.getText();
+		
+		boolean result = false;
+		
+		ArrayList<String> key = new ArrayList<String>();
+		ArrayList<String> val = new ArrayList<String>();
+
+		key.add("id");
+		val.add(id);
+		key.add("password");
+		val.add(password);
+		key.add("device");
+		val.add("m");
+		
+		try {
+			JSONObject jsonResult = HTTP.send("http://166.104.126.42:8090/NewSystem/signin.do", "post", key, val);
+			result = (boolean)jsonResult.get("result");
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(result) {
+			MainApp.initRootLayout();
+			MainApp.showSystemOverview();
+		}
+	}
 }
