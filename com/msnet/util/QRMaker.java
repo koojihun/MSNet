77af99a98164f344a84c953bc17ae45406abb5f5
Koo Ji Hoon <koo2788@naver.com>
2019-01-29 16:48:32 +0900

    koo

--- com/msnet/util/QRMaker.java
+++ com/msnet/util/QRMaker.java
@@ -2,13 +2,16 @@ package com.msnet.util;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Hashtable;
 
 import javax.imageio.ImageIO;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRMaker {
 	
@@ -37,13 +40,13 @@ public class QRMaker {
 			// 큐알코드 배경색상값
 			int backgroundColor = 0xFFFFFFFF;
 			
-			//Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
-			//hintMap.put(EncodeHintType.QR_VERSION, 13);
-			//hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+			Hashtable<EncodeHintType, Object> hintMap = new Hashtable<>();
+			hintMap.put(EncodeHintType.QR_VERSION, 13);
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 			QRCodeWriter qrCodeWriter = new QRCodeWriter();
 			
 			// 3,4번째 parameter값 : width/height값 지정
-			BitMatrix bitMatrix = qrCodeWriter.encode(codeurl, BarcodeFormat.QR_CODE, width, height);
+			BitMatrix bitMatrix = qrCodeWriter.encode(codeurl, BarcodeFormat.QR_CODE, width, height, hintMap);
 			MatrixToImageConfig matrixToImageConfig = new MatrixToImageConfig(qrcodeColor, backgroundColor);
 			BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix, matrixToImageConfig);
 			
